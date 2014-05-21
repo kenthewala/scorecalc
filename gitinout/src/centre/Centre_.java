0c0381b46ea54f3961ff274f111a213c173e1ae8
kenthewala <anoopsinghkhimani@gmail.com>
2014-05-21 22:14:36 +0530

    committing running code where tables are being generated and row is getting inserted (As of now a table contains only one row).

---
+++ gitinout/src/centre/Centre_.java
@@ -0,0 +1,14 @@
+package centre;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2014-05-17T13:35:39.361+0530")
+@StaticMetamodel(Centre.class)
+public class Centre_ {
+	public static volatile SingularAttribute<Centre, Integer> id;
+	public static volatile SingularAttribute<Centre, String> name;
+	public static volatile SingularAttribute<Centre, String> address;
+	public static volatile SingularAttribute<Centre, String> comments;
+}
